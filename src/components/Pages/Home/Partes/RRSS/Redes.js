c9e6f829d6cfcf52ba5a0af585b262dc3ca21437
Jriog <javierdelrioguerreiro@gmail.com>
2022-05-14 12:23:35 +0200

    grids

--- src/components/Pages/Home/Partes/RRSS/Redes.js
+++ src/components/Pages/Home/Partes/RRSS/Redes.js
@@ -3,15 +3,12 @@ import './Redes.css';
 
 function Redes() {
     return (
-        <div id='redes'>
-            <div class="container mx-auto">
-                <div class="grid grid-cols-4 gap-6">
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-
-                </div>
-            </div>
+        <div id='redes' class="h-56 grid grid-cols-5 gap-4 content-center ...">
+                <div></div>
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div></div>
         </div>
     );
 }
