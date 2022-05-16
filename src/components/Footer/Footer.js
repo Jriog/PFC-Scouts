57a5e2723ba8d6b591edbaa27e0fc28b4df63f50
Jriog <javierdelrioguerreiro@gmail.com>
2022-05-16 19:51:37 +0200

    foooteeeer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './Footer.css';
+import instagram from './instagram.png';
+import mail from './mail.png';
 
 function Footer() {
   return (
-    <div className='footer'>
-      <h1>Footer</h1>
+    <div className=''>
+      <div class="grid grid-rows-3 md:grid-rows-3">
+        <div class="grid grid-cols-9 gap-5">
+          <div></div>
+          <div></div>
+          <div class="referencia">¿Quienes somos?</div>
+          <div class="referencia">Secciones</div>
+          <div class="referencia">Mundo Scout</div>
+          <div class="referencia">Apúntate</div>
+          <div class="referencia">Tienda</div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="grid grid-cols-5 gap-4">
+          <div></div>
+          <div></div>
+
+          <div className='referencia'>
+          Nos puedes contactar a través de: <a href='https://www.instagram.com/sanjorge33/'><img src={instagram} alt="instagram"></img></a>   <a href='malito:grupo@sanjorge33.org'><img src={mail} alt="instagram"></img></a>
+          </div>
+          <div></div>
+          <div></div>
+
+        </div>
+        <div className='referencia'><a href=''>© 2022 Grupo Scout San Jorge 33</a></div>
+      </div>
     </div>
+  
   );
 }
 
--- src/components/Pages/Home/Partes/Descripcion/Descripcion.js
+++ src/components/Pages/Home/Partes/Descripcion/Descripcion.js
@@ -8,7 +8,7 @@ function Descripcion() {
       <div></div>
       <div className='texto'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</div>
       <div></div>
-      <div className='logodiv'><img src={foto} className='img'></img></div>
+      <div className='logodiv'><img src={foto} alt="flor de lis" className='img'></img></div>
       <div></div>
     </div>
   );
